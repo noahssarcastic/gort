22dbd428ab77b172a1b093b4f9c62d9f70804f62
Noah Iniguez <niniguezg@gmail.com>
2022-10-24 08:49:34 -0500

    Update tuple test to make pixmap

--- cmd/tupletest/main.go
+++ cmd/tupletest/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"fmt"
-
+	"github.com/noahssarcastic/tddraytracer/canvas"
+	"github.com/noahssarcastic/tddraytracer/color"
+	"github.com/noahssarcastic/tddraytracer/ppm"
 	"github.com/noahssarcastic/tddraytracer/tuple"
 )
 
@@ -21,17 +22,31 @@ func tick(env environment, proj projectile) projectile {
 }
 
 func main() {
-	proj := projectile{tuple.Point(0, 1, 0), tuple.Normalize(tuple.Vector(1, 1, 0))}
-	env := environment{tuple.Vector(0, -0.1, 0), tuple.Vector(-0.01, 0, 0)}
+	proj := projectile{tuple.Point(0, 1, 0), tuple.Normalize(tuple.Vector(1, 1, 0)).Multiply(4)}
+	env := environment{tuple.Vector(0, -0.1, 0), tuple.Vector(0, 0, 0)}
+
+	canv := canvas.New(200, 100)
 
 	tickCount := 0
 loop:
 	for {
 		tickCount += 1
 		proj = tick(env, proj)
-		fmt.Printf("Tick: %v; Position: %v\n", tickCount, proj.position)
+
+		// fmt.Printf("Tick: %v; Position: %v\n", tickCount, proj.position)
+
+		x := int(proj.position.X())
+		y := int(proj.position.Y())
+		if x >= 0 && x < canv.Width() && y >= 0 && y < canv.Height() {
+			println(x, y)
+			canv.SetPixel(x, (canv.Height()-1)-y, color.Red())
+
+		}
+
 		if proj.position.Y() <= 0 {
 			break loop
 		}
 	}
+
+	ppm.CanvasToPixmap(canv).WritePPM()
 }
