44e058f48e0f8c310006f0c498dadfd8d7dbfe36
Noah Iniguez <niniguezg@gmail.com>
2022-10-30 22:29:00 -0500

    Complete testmat script for visual test

--- cmd/testmat/main.go
+++ cmd/testmat/main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"math"
 
-	"github.com/noahssarcastic/tddraytracer/matrix"
+	"github.com/noahssarcastic/tddraytracer/canvas"
+	"github.com/noahssarcastic/tddraytracer/color"
+	"github.com/noahssarcastic/tddraytracer/ppm"
+	"github.com/noahssarcastic/tddraytracer/transform"
+	"github.com/noahssarcastic/tddraytracer/tuple"
 )
 
 func main() {
-	mat := matrix.Matrix{
-		[]float64{1, 2, 3},
-		[]float64{4, 5, 6},
+	canv := canvas.New(100, 100)
+	for i := 1; i <= 12; i++ {
+		pt := tuple.Point(1, 0, 0)
+		tform := transform.Chain(
+			transform.Scale(30, 0, 0),
+			transform.RotateZ(2.*math.Pi/12*float64(i)),
+			transform.Translate(float64(canv.Width())/2, float64(canv.Width())/2, 0),
+		)
+		pt = tform.Apply(pt)
+		x := int(pt.X())
+		y := int(pt.Y())
+		canv.SetPixel(x, (canv.Height()-1)-y, color.Red())
 	}
-	fmt.Println(mat)
+	ppm.CanvasToPixmap(canv).WritePPM()
 }
