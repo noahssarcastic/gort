b397566eebe930fb057c2e06faf50074d2ad38ae
Noah Iniguez <niniguezg@gmail.com>
2022-11-02 20:45:05 -0500

    Implement Ray.Transform()

--- ray/ray.go
+++ ray/ray.go
@@ -25,3 +25,10 @@ func (ray Ray) Direction() math.Tuple {
 func Position(ray Ray, t float64) math.Tuple {
 	return ray.origin.Add(ray.direction.Mult(t))
 }
+
+func (r Ray) Transform(mat math.Matrix) Ray {
+	return Ray{
+		origin:    mat.Apply(r.origin),
+		direction: mat.Apply(r.direction),
+	}
+}
--- ray/ray_test.go
+++ ray/ray_test.go
@@ -7,7 +7,12 @@ import (
 	"github.com/noahssarcastic/gort/math"
 )
 
-func TestPosition(t *testing.T) {
+func rayEqual(r1, r2 Ray) bool {
+	return math.TupleEqual(r1.origin, r2.origin) &&
+		math.TupleEqual(r1.direction, r2.direction)
+}
+
+func TestRayPosition(t *testing.T) {
 	r := NewRay(math.Point(2, 3, 4), math.Vector(1, 0, 0))
 	tests := []struct {
 		ray  Ray
@@ -27,5 +32,43 @@ func TestPosition(t *testing.T) {
 				t.Errorf("got %v, want %v", ans, tt.want)
 			}
 		})
+	}
+}
+
+func TestRayTransform(t *testing.T) {
+	tests := []struct {
+		r    Ray
+		mat  math.Matrix
+		want Ray
+	}{
+		{
+			NewRay(math.Point(1, 2, 3), math.Vector(0, 1, 0)),
+			math.Translate(3, 4, 5),
+			NewRay(math.Point(4, 6, 8), math.Vector(0, 1, 0)),
+		},
+		{
+			NewRay(math.Point(1, 2, 3), math.Vector(0, 1, 0)),
+			math.Scale(2, 3, 4),
+			NewRay(math.Point(2, 6, 12), math.Vector(0, 3, 0)),
+		},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("%v", i), func(t *testing.T) {
+			ans := tt.r.Transform(tt.mat)
+			if !rayEqual(tt.want, ans) {
+				t.Errorf("want %v; got %v", tt.want, ans)
+			}
+		})
+	}
+}
+
+func TestRayTransform_no_modify(t *testing.T) {
+	r := NewRay(math.Point(1, 2, 3), math.Vector(0, 1, 0))
+	mat := math.Translate(3, 4, 5)
+	want := NewRay(math.Point(1, 2, 3), math.Vector(0, 1, 0))
+	r.Transform(mat)
+	got := r
+	if !rayEqual(want, got) {
+		t.Errorf("want %v; got %v", want, got)
 	}
 }
